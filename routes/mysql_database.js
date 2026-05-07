dd9c8b4597a012871a10ced72d0dd73c451c953a
Nafisa Ebrahimi <nafisai2750@gmail.com>
2026-05-07 18:46:51 +0430

    work on database file

--- routes/mysql_database.js
+++ routes/mysql_database.js
@@ -1,8 +1,24 @@
-const mysql=require('mysql2')
+const mysql = require("mysql2");
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'123321',
-    database:'nafisastyle'
-}).promise();
+const pool = mysql
+  .createPool({
+    host: "localhost",
+    user: "root",
+    password: "123321",
+    database: "nafisastyle",
+  })
+  .promise();
+
+const getCourses = async () => {
+  const [reuslt] = await pool.query("select * from courses");
+ return reuslt
+};
+const getCourse = async (id) => {
+  const [reuslt] = await pool.query(`select * from courses where id=? `,[id]);
+ return reuslt
+};
+
+const data=getCourse(1).then((reuslt)=>{
+console.log(reuslt);
+})
+
